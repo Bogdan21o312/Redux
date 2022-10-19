0495dca54d4f50daa1661bd36657e1882f2d75ec
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-19 20:27:56 +0300

    Todo checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import "./index.css"
 
 // interface Todo {
 //     id:  any,
@@ -33,6 +34,20 @@ const App = () => {
         setTodos(todos.filter(todo => todo.id !== todoId))
     }
 
+    const toggleTodoComplete = (todoId) => {
+        setTodos(
+            todos.map(
+                todo => {
+                    if (todo.id !== todoId) return todo
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+            )
+        )
+    }
+
     return (
         <div>
             <label>
@@ -40,10 +55,15 @@ const App = () => {
                 <button onClick={addTodo}>Add Todo</button>
                 <ul>
                     {todos.map(todo => <li key={todo.id}>
-                        <input type="checkbox"/>
+                        <input
+                            className="checkbox"
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => toggleTodoComplete(todo.id)}
+                        />
                         <span>{todo.text}</span>
                         <span
-                            style={{color: "red"}}
+                            className="times"
                             onClick={() => removeTodo(todo.id)}
                         >&times;</span>
                     </li>)}
